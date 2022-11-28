9395125f43ebdf533677d7e270ddb9cd2afc8d34
차요셉 <jscha@raoncorp.com>
2022-11-28 14:44:58 +0900

    feat: ColorSet 추가
    #16
    - Ticlemoa(App) 모듈의  Assets.xcassets 파일에 ColorSet추가 및 UserInface 모듈을 타게팅함(inspector 영역 target membership 체크)
    - 추가한 ColorSet에 대한 Color Extension 추가

---
+++ Targets/UserInterface/Sources/Utils/Extensions/Color+CustomColor.swift
@@ -0,0 +1,20 @@
+//
+//  Color+CustomColor.swift
+//  UserInterface
+//
+//  Created by Joseph Cha on 2022/11/26.
+//  Copyright © 2022 nyongnyong. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static var black: Color { return Color("black") }
+    static var grey1: Color { return Color("grey1") }
+    static var grey2: Color { return Color("grey2") }
+    static var grey2Line: Color { return Color("grey2Line") }
+    static var grey3: Color { return Color("grey3") }
+    static var grey4: Color { return Color("grey4") }
+    static var primary: Color { return Color("primary") }
+    static var white: Color { return Color("white") }
+}
