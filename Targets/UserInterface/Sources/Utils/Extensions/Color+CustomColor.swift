4c28fbaf40b8dfd284ee6dae50c9769be9a7f7f2
차요셉 <jscha@raoncorp.com>
2022-12-04 18:45:43 +0900

    fix: Color-Extension 관련 피리뷰 실행 실패 처리
    #33
     - Asset파일의 colorSet명 변경 black, white, primary -> ticlemoadBlack, ticlemoaWhite, ticlemoaPrimary
     - Color Extension 프로퍼티명 변경 black, white, primary -> ticlemoadBlack, ticlemoaWhite, ticlemoaPrimary

--- Targets/UserInterface/Sources/Utils/Extensions/Color+CustomColor.swift
+++ Targets/UserInterface/Sources/Utils/Extensions/Color+CustomColor.swift
@@ -9,12 +9,12 @@
 import SwiftUI
 
 extension Color {
-    static var black: Color { return Color("black") }
     static var grey1: Color { return Color("grey1") }
     static var grey2: Color { return Color("grey2") }
     static var grey2Line: Color { return Color("grey2Line") }
     static var grey3: Color { return Color("grey3") }
     static var grey4: Color { return Color("grey4") }
-    static var primary: Color { return Color("primary") }
-    static var white: Color { return Color("white") }
+    static var ticlemoaBlack: Color { return Color("ticlemoaBlack") }
+    static var ticlemoaPrimary: Color { return Color("ticlemoaPrimary") }
+    static var ticlemoaWhite: Color { return Color("ticlemoaWhite") }
 }
