68c3dc204dc6ab54a78e70af75159c332fee31e1
차요셉 <jscha@raoncorp.com>
2022-12-12 11:49:00 +0900

    feat: 링크 추가 UI 구성 #26

---
+++ Targets/UserInterface/Sources/LinkAdd/ViewModel/LinkAddViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  LinkAddViewModel.swift
+//  Ticlemoa
+//
+//  Created by Joseph Cha on 2022/12/09.
+//  Copyright © 2022 nyongnyong. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+final class LinkAddViewModel: ObservableObject {
+    @Published var link: String = ""
+    @Published var articleTitle: String = "아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목"
+    @Published var articleThumbNail: String = "ArticlePlaceholder"
+    @Published var memo: String = ""
+    @Published var isPublicSetting: Bool = false
+    @Published var tags: [String] = ["태그명"]
+    
+    init() {
+        if let copiedlink = UIPasteboard.general.string {
+            self.link = copiedlink
+        }
+    }
+}
