9d2090e0e0761b6dfccde1008aacca2b9ab3205d
Shin <struct.developer@gmail.com>
2022-12-20 16:42:47 +0900

    feat: BottomSheet 구현
    - #64
    - BottomSheet 구현, ticlemoaBottomSheet function 구현
    - 하단 pan gesture 구현, 상단 pan gesture 미구현

---
+++ Targets/UserInterface/Sources/Utils/BottomSheetView.swift
@@ -0,0 +1,98 @@
+//
+//  BottomSheetView.swift
+//  UserInterface
+//
+//  Created by Shin Jae Ung on 2022/12/20.
+//  Copyright © 2022 nyongnyong. All rights reserved.
+//
+
+import SwiftUI
+
+struct BottomSheetView<Content: View>: View {
+    @Binding private var isPresent: Bool
+    @State private var backgroundOpacity: Double = 0
+    @State private var viewYOffset: Double = UIScreen.main.bounds.height
+    @Environment(\.dismiss) var dismiss
+    var content: () -> (Content)
+
+    init(isPresent: Binding<Bool>, @ViewBuilder content: @escaping () -> (Content)) {
+        self._isPresent = isPresent
+        self.content = content
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.black.opacity(backgroundOpacity)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    dismissWithAnimation()
+                }
+            
+            GeometryReader { geometry in
+                VStack(spacing: 0) {
+                    Spacer()
+                    
+                    VStack(spacing: 0) {
+                        Capsule()
+                            .frame(width: 42, height: 4)
+                            .padding(.top, 18)
+                            .padding(.bottom, 4)
+                        
+                        content()
+                        
+                        Spacer()
+                            .frame(height: geometry.safeAreaInsets.bottom)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(Color.grey1)
+                    .offset(x: 0, y: viewYOffset)
+                    .cornerRadius(20, corners: .topLeft)
+                    .cornerRadius(20, corners: .topRight)
+                    .gesture(drag)
+                }
+                .ignoresSafeArea(.all, edges: [.bottom])
+            }
+        }
+        .onAppear {
+            identicalWithAnimation()
+        }
+    }
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                if value.translation.height > 0 {
+                    viewYOffset = value.translation.height
+                } else {
+                    // TODO: implement if you want to stratch
+                }
+            }
+            .onEnded { value in
+                if value.translation.height <= -50 {
+                    // TODO: implement if you want to stratch
+                } else if value.translation.height >= 50 {
+                    dismissWithAnimation()
+                } else {
+                    identicalWithAnimation()
+                }
+            }
+    }
+    
+    private func identicalWithAnimation() {
+        withAnimation {
+            viewYOffset = 0
+            backgroundOpacity = 0.4
+        }
+    }
+    
+    private func dismissWithAnimation() {
+        withAnimation {
+            backgroundOpacity = 0
+            isPresent = false
+            viewYOffset = UIScreen.main.bounds.height
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                dismiss()
+            }
+        }
+    }
+}
