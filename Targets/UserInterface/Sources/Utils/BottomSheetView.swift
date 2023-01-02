3b12fdcb82bb476b8dd2f91ef0962fcc4d2da54f
Shin <struct.developer@gmail.com>
2023-01-02 15:51:36 +0900

    refactor: ButtomSheetView 나누기, 접근제어자 수정
    - #64
    - 접근제어자를 다듬었습니다.
    - ShareExtensionViewController에 적용하기 위해 필요한 부분을 public 처리하였습니다.

--- Targets/UserInterface/Sources/Utils/BottomSheetView.swift
+++ Targets/UserInterface/Sources/Utils/BottomSheetView.swift
@@ -12,9 +12,9 @@ struct BottomSheetView<Content: View>: View {
     @Binding private var isPresent: Bool
     @State private var backgroundOpacity: Double = 0
     @State private var viewYOffset: Double = UIScreen.main.bounds.height
-    @Environment(\.dismiss) var dismiss
-    var content: () -> (Content)
-    let isHandlebarHidden: Bool
+    @Environment(\.dismiss) private var dismiss
+    private var content: () -> (Content)
+    private let isHandlebarHidden: Bool
 
     init(isPresent: Binding<Bool>, withHandleBar: Bool = false, @ViewBuilder content: @escaping () -> (Content)) {
         self._isPresent = isPresent
@@ -29,39 +29,16 @@ struct BottomSheetView<Content: View>: View {
                 .onTapGesture {
                     dismissWithAnimation()
                 }
-            
-            GeometryReader { geometry in
-                VStack(spacing: 0) {
-                    Spacer()
-                    
-                    VStack(spacing: 0) {
-                        Capsule()
-                            .frame(width: 42, height: 4)
-                            .padding(.top, 18)
-                            .padding(.bottom, 4)
-                            .foregroundColor(isHandlebarHidden ? .clear : .ticlemoaBlack)
-                        
-                        content()
-                        
-                        Spacer()
-                            .frame(height: geometry.safeAreaInsets.bottom)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .background(Color.grey1)
-                    .cornerRadius(20, corners: .topLeft)
-                    .cornerRadius(20, corners: .topRight)
-                    .offset(x: 0, y: viewYOffset)
-                    .gesture(drag)
-                }
-                .ignoresSafeArea(.all, edges: [.bottom])
-            }
+            BottomSheet(content: content)
+            .offset(x: 0, y: viewYOffset)
+            .gesture(drag)
         }
         .onAppear {
             identicalWithAnimation()
         }
     }
     
-    var drag: some Gesture {
+    private var drag: some Gesture {
         DragGesture()
             .onChanged { value in
                 if value.translation.height > 0 {
@@ -96,6 +73,42 @@ struct BottomSheetView<Content: View>: View {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                 dismiss()
             }
+        }
+    }
+}
+
+public struct BottomSheet<Content: View>: View {
+    private var content: () -> (Content)
+    private let isHandlebarHidden: Bool
+    
+    public init(withHandleBar: Bool = false, @ViewBuilder content: @escaping () -> (Content)) {
+        self.isHandlebarHidden = !withHandleBar
+        self.content = content
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Capsule()
+                        .frame(width: 42, height: 4)
+                        .padding(.top, 18)
+                        .padding(.bottom, 4)
+                        .foregroundColor(isHandlebarHidden ? .clear : .ticlemoaBlack)
+                    
+                    content()
+                    
+                    Spacer()
+                        .frame(height: geometry.safeAreaInsets.bottom)
+                }
+                .frame(maxWidth: .infinity)
+                .background(Color.grey1)
+                .cornerRadius(20, corners: .topLeft)
+                .cornerRadius(20, corners: .topRight)
+            }
+            .ignoresSafeArea(.all, edges: [.bottom])
         }
     }
 }
