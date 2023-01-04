48ff3ee252bd1753c71433fce9e205117096c84c
marco <keepingitflow@gmail.com>
2023-01-04 18:18:16 +0900

    refactor: DTO 동작 구현
    - Domain 에서 일어나는 동작이므로 주체를 Domain Entity로 작성
    - 단순 accessToken 은 문자열 자체로 전달

--- Targets/Domain/Sources/Model/ArticleModel.swift
+++ Targets/Domain/Sources/Model/ArticleModel.swift
@@ -11,6 +11,21 @@ import DomainInterface
 
 import Foundation
 
+extension Article {
+    
+    func uploadArticleRequest(with accessToken: String) -> UploadArticleRequest {
+        .init(
+            accessToken: accessToken,
+            content: self.content,
+            title: self.title,
+            url: self.url,
+            isPublic: self.isPublic,
+            tagIds: self.tagIds
+        )
+    }
+    
+}
+
 public final class ArticleModel: ArticleModelProtocol {
     
     @Published public var items: [Article] = []
@@ -37,19 +52,9 @@ extension ArticleModel {
     }
     
     public func update(_ item: Article) async {
-        let request = UploadArticleRequest(
-            accessToken: "",
-            content: item.content,
-            title: item.title,
-            url: item.url,
-            isPublic: item.isPublic,
-            tagIds: item.tagIds
-        )
-        guard let data = await api.uploadArticle(by: request) else {
-            return
-        }
-        
+        let uploadArticleRequest = item.uploadArticleRequest(with: "accessToken") // TODO: 전달방법 고민 필요 UserDefault?
         do {
+            let data = try await api.request(by: uploadArticleRequest)
             let response = try JSONDecoder().decode(UploadArticleResponse.self, from: data)
         } catch {
             print(error.localizedDescription)
