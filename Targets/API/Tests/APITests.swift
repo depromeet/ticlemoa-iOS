fc280a27892bd66cc806eaecf2b7276548d523ea
marco <keepingitflow@gmail.com>
2023-01-07 19:04:42 +0900

    test: Article 관련 API 동작 UnitTest
    - Create
    - Read, Search
    - Update
    - Delete

--- Targets/API/Tests/APITests.swift
+++ Targets/API/Tests/APITests.swift
@@ -9,6 +9,159 @@
 import XCTest
 @testable import API
 
-final class APITests: XCTestCase {
-
+extension APITests {
+    var accessToken: String { URLStrings.adminAccesstoken }
+    var userId: Int { 1 }
 }
+
+final class APITests: XCTestCase {
+    var sut: APIDetails!
+        
+    override func setUp() {
+        self.sut = TiclemoaAPI()
+    }
+    
+    override func tearDown() {
+        self.sut = nil
+    }
+    
+    func test_ticlemoaAPI_request() async {
+        // given
+        let request = ReadArticleRequest(accessToken: accessToken, userId: userId)
+        // when
+        let result = await self.sut.request(by: request)
+        
+        var success: Bool
+        switch result {
+            case .success(_): success = true
+            case .failure(_): success = false
+        }
+        
+        // then
+        XCTAssertTrue(success)
+    }
+    
+}
+
+// MARK: - Article Request / Response
+/*
+    사용자 정보 userId = 0
+    
+    article 의 id 넣어서 각 테스트 가능
+    delete 테스트는 id를 확인 후 주석 해제하고 테스트
+ */
+extension APITests {
+    
+    func test_createArticle() async {
+        // given
+        let newItem = CreateArticleRequest.Body.init(
+            content: "테스트 용 업로드",
+            userId: userId,
+            title: "김용우 블로그",
+            url: "https://keeplo.tistory.com/",
+            isPublic: false,
+            tagIds: []
+        )
+        let request = CreateArticleRequest(accessToken: accessToken, body: newItem)
+        // when
+        let result = await self.sut.request(by: request)
+        if case .success(let data) = result {
+            let response = try? JSONDecoder().decode(CreateArticleResponse.self, from: data)
+
+        // then
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+        } else {
+            XCTAssertTrue(false)
+        }
+
+    }
+
+    func test_readArticle() async {
+        // given
+        let request = ReadArticleRequest(accessToken: accessToken, userId: userId)
+        // when
+        let result = await self.sut.request(by: request)
+        if case .success(let data) = result {
+            let response = try? JSONDecoder().decode(ReadArticleResponse.self, from: data)
+            
+        // then
+            XCTAssertNotNil(data)
+            XCTAssertTrue(response!.articles.count > 10)
+        } else {
+            XCTAssertTrue(false)
+        }
+    }
+    
+    func test_uploadArticle() async {
+        // given
+        let editedTitle = "김용우 블로그"
+        let request = UpdateArticleRequest(
+            accessToken: accessToken,
+            articleId: 41,
+            body: .init(
+                content: "테스트 블로그",
+                userId: 1,
+                title: editedTitle,
+                url: "https://keeplo.tistory.com/",
+                isPublic: false,
+                tagIds: []
+            )
+        )
+        // when
+        let result = await self.sut.request(by: request)
+
+        if case .success(let data) = result {
+            let response = try? JSONDecoder().decode(UpdateArticleResponse.self, from: data)
+
+        // then
+            XCTAssertNotNil(data)
+            XCTAssertEqual(response!.title, editedTitle)
+        } else {
+            XCTAssertTrue(false)
+        }
+    }
+    
+//    func test_deleteArticle() async {
+//        // given
+//        let request = DeleteArticleRequest(accessToken: accessToken, articleIds: [
+//            "\(31)"
+//        ])
+//        // when
+//        let result = await self.sut.request(by: request)
+//        if case .success(let data) = result {
+//
+//        // then
+//            XCTAssertNotNil(data)
+//        } else {
+//            XCTAssertTrue(false)
+//        }
+//    }
+
+    func test_searchArticle() async {
+        // given
+        let request = SearchArticleRequest(accessToken: accessToken, search: "1")
+        // when
+        let result = try! await self.sut.request(by: request)
+        if case .success(let data) = result {
+            let response = try? JSONDecoder().decode(SearchArticleResponse.self, from: data)
+            
+        // then
+            XCTAssertNotNil(data)
+            XCTAssertTrue(response!.articles.count > 2)
+        } else {
+            XCTAssertTrue(false)
+        }
+    }
+    
+}
+
+// MARK: - Article Request / Response
+/*
+    
+ */
+
+extension APITests {
+    
+}
+
