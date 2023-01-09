768a362bb2bf74e923daa8048981067b7f869468
KimWooSung <kipsong133@gmail.com>
2023-01-10 00:14:08 +0900

    feat: AccessToken 획득 API 구현

--- Targets/DomainInterface/Sources/Model/LoginModelProtocol.swift
+++ Targets/DomainInterface/Sources/Model/LoginModelProtocol.swift
@@ -13,4 +13,5 @@ public protocol LoginModelProtocol {
     func checkKakaoLogin() async -> Bool
     func isKakaoTalkLoginUrl(_ url: URL) -> Bool
     func authController(url: URL) -> Bool
+    func requestAccessToken() async -> (String, Int)?
 }
