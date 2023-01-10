ff204db53cb50eeb80dce7a5a00e967fc105ef8c
Shin <struct.developer@gmail.com>
2023-01-10 21:19:27 +0900

    fix: 복사한 링크 추가하기, 검색하기 back버튼 동작하지 않는 버그 해결
    - #165
    - MainTabView의 TabView 상위에 NavigationView를 Embed하여 해결

--- Targets/UserInterface/Sources/Scenes/MainTab/MainTabView.swift
+++ Targets/UserInterface/Sources/Scenes/MainTab/MainTabView.swift
@@ -18,64 +18,67 @@ struct MainTabView: View {
     }
     
     var body: some View {
-        TabView {
-            NavigationView {
-                HomeView(viewModel: HomeViewModel(modelContainer: modelContainer))
-            }
-            .tabItem {
-                Tab.home.imageItem
-                Tab.home.textItem
+        NavigationView {
+            TabView {
+                NavigationView {
+                    HomeView(viewModel: HomeViewModel(modelContainer: modelContainer))
+                }
+                .tabItem {
+                    Tab.home.imageItem
+                    Tab.home.textItem
+                }
+                .overlay {
+                    VStack {
+                        Spacer()
+                        if isSnackBarButtonExisting {
+                            NavigationLink(
+                                destination: AddingLinkView(modelContainer: modelContainer, fromWhere: .snackBar),
+                                label: {
+                                    HStack {
+                                        Text("복사한 링크 추가하기")
+                                            .font(.system(size: 14))
+                                            .foregroundColor(.grey1)
+                                            .padding(.leading, 16)
+                                        Spacer()
+                                        Button {
+                                            isSnackBarButtonExisting = false
+                                        } label: {
+                                            Image("CloseButton")
+                                                .frame(width: 13.67, height: 13.67)
+                                                .padding(.trailing, 15.19)
+                                        }
+                                    }
+                                    .frame(height: 44)
+                                    .background(Color.ticlemoaBlack)
+                                })
+                            .cornerRadius(6.0)
+                            .padding(.horizontal, 32)
+                            .padding(.bottom, 32)
+                        }
+                    }
+                }
+                NavigationView {
+                    MyPageView(viewModel: .init(modelContainer: modelContainer, isLogin: $isLogin))
+                }
+                .tabItem {
+                    Tab.myPage.imageItem
+                    Tab.myPage.textItem
+                }
             }
             .overlay {
                 VStack {
                     Spacer()
-                    if isSnackBarButtonExisting {
-                        NavigationLink(
-                            destination: AddingLinkView(modelContainer: modelContainer, fromWhere: .snackBar),
-                            label: {
-                                HStack {
-                                    Text("복사한 링크 추가하기")
-                                        .font(.system(size: 14))
-                                        .foregroundColor(.grey1)
-                                        .padding(.leading, 16)
-                                    Spacer()
-                                    Button {
-                                        isSnackBarButtonExisting = false
-                                    } label: {
-                                        Image("CloseButton")
-                                            .frame(width: 13.67, height: 13.67)
-                                            .padding(.trailing, 15.19)
-                                    }
-                                }
-                                .frame(height: 44)
-                                .background(Color.ticlemoaBlack)
-                            })
-                        .cornerRadius(6.0)
-                        .padding(.horizontal, 32)
-                        .padding(.bottom, 32)
-                    }
+                    NavigationLink(
+                        destination: AddingLinkView(modelContainer: modelContainer, fromWhere: .naviBar),
+                        label: {
+                            Image("add")
+                        })
+                    Spacer()
+                        .frame(height: 16)
                 }
             }
-            NavigationView {
-                MyPageView(viewModel: .init(modelContainer: modelContainer, isLogin: $isLogin))
-            }
-            .tabItem {
-                Tab.myPage.imageItem
-                Tab.myPage.textItem
-            }
         }
-        .overlay {
-            VStack {
-                Spacer()
-                NavigationLink(
-                    destination: AddingLinkView(modelContainer: modelContainer, fromWhere: .naviBar),
-                    label: {
-                        Image("add")
-                    })
-                Spacer()
-                    .frame(height: 16)
-            }
-        }
+        
     }
 }
 
