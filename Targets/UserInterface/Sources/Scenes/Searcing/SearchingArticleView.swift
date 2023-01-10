c6f106508f3e622494c6f202e2e68a4f717ede71
marco <keepingitflow@gmail.com>
2023-01-10 09:09:17 +0900

    feat: 검색 기능 및 최근 검색어 관리 기능 구현
    - 최근 검색어 Row 구현 (삭제기능 및 선택기능)
    - 검색요청과 결과에 따른 화면 변화 구현

--- Targets/UserInterface/Sources/Scenes/Searcing/SearchingArticleView.swift
+++ Targets/UserInterface/Sources/Scenes/Searcing/SearchingArticleView.swift
@@ -7,18 +7,17 @@
 //
 
 import SwiftUI
-//import Foundation
 
 struct SearchingArticleView: View {
     @ObservedObject var viewModel: SearchingArticleViewModel
-    @State private var searchQuery: String = ""
-//    @AppStorage("searchQueries") var recentQueries: [String] = [] MARK: 사용법 학습필요
-    @State var recentQueries: [String] = (UserDefaults.standard.array(forKey: "searchQueries") as? [String] ?? []) {
+    @State var recentQueries: [String] = (UserDefaults.standard
+        .array(forKey: "searchQueries") as? [String] ?? []
+    ) {
         willSet {
             UserDefaults.standard.set(newValue, forKey: "searchQueries")
         }
     }
-    @Environment(\.dismiss) private var dismiss
+    @Environment(\.presentationMode) private var presentationMode
     
     init(viewModel: SearchingArticleViewModel) {
         self.viewModel = viewModel
@@ -27,59 +26,115 @@ struct SearchingArticleView: View {
     var body: some View {
         ZStack {
             VStack(spacing: 32) {
-                HStack(spacing: 16.19) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image("arrow")
+                searchBar
+                if case let .searched(items) = viewModel.state {
+                    VStack(spacing: 0) {
+                        HStack {
+                            Text("총 \(items.count)개의 검색결과")
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
+                        .padding(.leading, 20)
+                        List {
+                            ForEach(items, id: \.id) { item in
+                                ArticleRow(title: item.title, imageURLString: "")
+                            }
+                        }
+                        .listStyle(.plain)
+                        .frame(maxWidth: .infinity)
+                        .onTapGesture {
+                            UIApplication.shared.sendAction(
+                                #selector(UIResponder.resignFirstResponder),
+                                to: nil,
+                                from: nil,
+                                for: nil
+                            )
+                        }
                     }
-                    searchBar
-                }
-                .padding(.leading, 24.52)
-                .padding(.trailing, 20)
-                if searchQuery.isEmpty {
-                    recentQueryList
                 } else {
-                    articleList
+                    recentQueryList
                 }
                 Spacer()
             }
-            if recentQueries.isEmpty && searchQuery.isEmpty {
-                Text("최근 검색어가 없습니다.")
-                    .font(.system(size: 14))
-                    .foregroundColor(.grey4)
+            if case let .searched(items) = viewModel.state,
+               items.isEmpty {
+                VStack {
+                    Image("Nothing")
+                    Text("검색 결과가 없습니다")
+                        .font(.system(size: 14))
+                        .foregroundColor(.grey4)
+                }
+            } else if recentQueries.isEmpty {
+                VStack {
+                    Image("Nothing")
+                    Text("최근 검색어가 없습니다")
+                        .font(.system(size: 14))
+                        .foregroundColor(.grey4)
+                }
             }
         }
+        .setupBackground()
         .hideKeyboard()
+        .onChange(of: viewModel.searchQuery) { searchQuery in
+            if searchQuery.isEmpty {
+                viewModel.state = .idle
+            }
+        }
+        .onReceive(viewModel.$searchQuery.debounce(for: 3, scheduler: RunLoop.main)) { query in
+            Task {
+                guard let searchedQuery = await self.viewModel.submit(by: query) else { return }
+                self.recentQueries.insert(searchedQuery, at: 0)
+            }
+        }
     }
 }
 
 extension SearchingArticleView {
+    
     var searchBar: some View {
-        HStack(spacing: 0) {
-            Image("search")
-                .padding(.leading, 11.5)
-            TextField("", text: $searchQuery)
-                .placeholder("검색어를 입력해주세요", when: searchQuery.isEmpty, color: .grey3)
-                .font(.system(size: 14))
-                .padding(.leading, 6.11)
-                .submitLabel(.done)
-            if !searchQuery.isEmpty {
-                Button {
-                    searchQuery = ""
-                } label: {
-                    Image("ClearButtonGreyLine2")
-                        .frame(width: 16.5, height: 16.5)
-                        .padding(.leading, 8.75)
-                        .padding(.trailing, 12.75)
+        HStack(spacing: 16.19) {
+            Button {
+                self.presentationMode.wrappedValue.dismiss()
+            } label: {
+                Image("arrow")
+            }
+            HStack(spacing: 0) {
+                Image("search")
+                    .padding(.leading, 11.5)
+                TextField("", text: $viewModel.searchQuery)
+                    .placeholder(
+                        "검색어를 입력해주세요",
+                        when: viewModel.searchQuery.isEmpty,
+                        color: .grey3
+                    )
+                    .font(.system(size: 14))
+                    .padding(.leading, 6.11)
+                    .submitLabel(.done)
+                    .onSubmit {
+                        Task {
+                            guard let searchedQuery = await self.viewModel.submit() else { return }
+                            self.recentQueries.insert(searchedQuery, at: 0)
+                        }
+                    }
+                if !viewModel.searchQuery.isEmpty {
+                    Button {
+                        viewModel.searchQuery = ""
+                    } label: {
+                        Image("ClearButtonGreyLine2")
+                            .frame(width: 16.5, height: 16.5)
+                            .padding(.leading, 8.75)
+                            .padding(.trailing, 12.75)
+                    }
                 }
             }
+            .frame(height: 42)
+            .background(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(Color.grey2Line, lineWidth: 1)
+            )
         }
-        .frame(height: 42)
-        .background(
-            RoundedRectangle(cornerRadius: 6)
-                .stroke(Color.grey2Line, lineWidth: 1)
-        )
+        .padding(.leading, 24.52)
+        .padding(.trailing, 20)
     }
     
     var recentQueryList: some View {
@@ -89,18 +144,23 @@ extension SearchingArticleView {
                     .font(.system(size: 14))
                 Spacer()
                 Button {
-                    recentQueries.removeAll()
+                    self.recentQueries = []
                 } label: {
                     Text("전체삭제")
                         .foregroundColor(.grey3)
                         .font(.system(size: 12))
                 }
             }
-            .padding(.horizontal, 20)
+            .padding([.horizontal, .bottom], 20)
             ScrollView {
                 LazyVStack {
-                    ForEach(recentQueries, id: \.self) { recentQuery in
-                        recentQueryView(recentQueries: $recentQueries, query: recentQuery)
+                    ForEach(Array(zip(recentQueries.indices, recentQueries)), id: \.0) { index, recentQuery in
+                        RecentQueryRow(
+                            recentQuery: recentQuery,
+                            searchAction: { self.viewModel.searchQuery = $0 },
+                            removeAction: { self.recentQueries.remove(at: index) }
+                        )
+                        .listRowBackground(Color.clear)
                     }
                 }
             }
@@ -110,55 +170,6 @@ extension SearchingArticleView {
         }
     }
     
-    var articleList: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text("총 \(4)개의 검색결과")
-                    .font(.system(size: 14))
-                Spacer()
-            }
-            .padding(.leading, 20)
-            
-            // FIXME: 일단 HomeView의 HomeArticleList와 동일하게 구현, 추후에 ArticleRow 수정에 따라 변경 예정
-            List {
-//                ArticleRow()
-//                ArticleRow()
-//                ArticleRow()
-//                ArticleRow()
-//                ArticleRow()
-            }
-            .listStyle(.plain)
-            .frame(maxWidth: .infinity)
-            .onTapGesture {
-                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-            }
-        }
-    }
-}
-
-private struct recentQueryView: View {
-    @Binding var recentQueries: [String]
-    let query: String
-    
-    var body: some View {
-        HStack(spacing: 0) {
-            Image("recent")
-                .frame(width: 14, height: 14)
-            Text(query)
-                .font(.system(size: 16))
-                .padding(.leading, 9)
-            Spacer()
-            Button {
-                recentQueries = recentQueries.filter({ $0 != query })
-            } label: {
-                Image("deleteButton")
-                    .frame(width: 10.5, height: 10.5)
-            }
-        }
-        .frame(height: 56)
-        .padding(.leading, 23)
-        .padding(.trailing, 21.75)
-    }
 }
 
 struct SearchingArticleView_Previews: PreviewProvider {
