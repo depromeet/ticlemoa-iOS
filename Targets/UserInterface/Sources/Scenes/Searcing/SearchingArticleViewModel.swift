5cd06a90758e001c65be39468b24400db7683080
marco <keepingitflow@gmail.com>
2023-01-10 09:06:49 +0900

    feat: SearchingViewModel 수정
    - 상태값 관리 State 열거형 추가
    - 검색어 체크 (빈 공백만 있는 검색어)
    - 제출 동작 추가
    - 검색 동작 추가

--- Targets/UserInterface/Sources/Scenes/Searcing/SearchingArticleViewModel.swift
+++ Targets/UserInterface/Sources/Scenes/Searcing/SearchingArticleViewModel.swift
@@ -7,12 +7,54 @@
 //
 
 import SwiftUI
+import DomainInterface
 
 final class SearchingArticleViewModel: ObservableObject {
+    
+    enum State {
+        case idle
+        case searched([Article])
+    }
+    
     @ObservedObject var modelContainer: ModelContainer
+    @Published var state: State = .idle
+    @Published var searchQuery: String = ""
     
     init(modelContainer: ModelContainer) {
         self.modelContainer = modelContainer
     }
+    
+}
+
+extension SearchingArticleViewModel {
+    
+    func submit(by query: String? = nil) async -> String? {
+        self.state = .idle
+        guard let checkedQuery = checkQuery(by: query) else { return nil }
+        await self.search(by: checkedQuery)
+        return checkedQuery
+    }
+
+}
+
+extension SearchingArticleViewModel {
+    
+    private func search(by query: String) async {
+        let result = await self.modelContainer.articleModel.search(query)
+        self.state = .searched(result)
+    }
+    
+    private func checkQuery(by query: String? = nil) -> String? {
+        if let keyword = query,
+           !keyword.filter({ $0 != " " }).isEmpty
+        {
+            return keyword
+        } else if !searchQuery.filter({ $0 != " " }).isEmpty {
+            return self.searchQuery
+        } else {
+            return nil
+        }
+    }
+    
 }
 
