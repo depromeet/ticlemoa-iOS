271ba592cb3a71d011c8eba5c39f7c8bc0cdb995
Keeplo <keepingitflow@gmail.com>
2023-01-12 07:32:53 +0900

    chore: 프로젝트 환경 변경
    - tuist 라이브러리 추가 제거
    - kakao 관련 코드 주석처리

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,14 +9,14 @@ import ProjectDescription
 
 let dependencies = Dependencies(
 	swiftPackageManager: [
-		.remote(
-			url: "https://github.com/apple/swift-collections.git",
-			requirement: .upToNextMajor(from: "1.0.0")
-		),
-		.remote(
-			url: "https://github.com/kakao/kakao-ios-sdk.git",
-			requirement: .upToNextMajor(from: "2.0.0")
-		)
+//		.remote(
+//			url: "https://github.com/apple/swift-collections.git",
+//			requirement: .upToNextMajor(from: "1.0.0")
+//		),
+//		.remote(
+//			url: "https://github.com/kakao/kakao-ios-sdk.git",
+//			requirement: .upToNextMajor(from: "2.0.0")
+//		)
 	],
 	platforms: [.iOS]
 )
