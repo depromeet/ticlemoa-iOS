9cf46ca0bf82283e6db716a3c78280770319268c
Keeplo <keepingitflow@gmail.com>
2023-01-13 00:58:18 +0900

    fix: 아티클추가 제목 플레이스 홀더 제거

--- Targets/UserInterface/Sources/AddingLinkView/ViewModel/AddingLinkViewModel.swift
+++ Targets/UserInterface/Sources/AddingLinkView/ViewModel/AddingLinkViewModel.swift
@@ -16,7 +16,7 @@ final class AddingLinkViewModel: ObservableObject {
     @ObservedObject var modelContainer: ModelContainer
     
     @Published var link: String = ""
-    @Published var articleTitle: String = "아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목아티클제목"
+    @Published var articleTitle: String = ""
     @Published var articleThumbNail: String = "ArticlePlaceholder"
     @Published var memo: String = ""
     @Published var isPublicSetting: Bool = false
