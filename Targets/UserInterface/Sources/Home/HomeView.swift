2f3090ee3397df0e7dd3215da47fefa2178c3215
Shin <struct.developer@gmail.com>
2023-01-14 01:04:56 +0900

    feat: 홈 화면 태그 가로스크롤 기능 적용
    - #231
    - 홈 화면 태그 세로스크롤 기능 동작하지 않도록 함
    - 홈 화면 태그 가로스크롤 기능

--- Targets/UserInterface/Sources/Home/HomeView.swift
+++ Targets/UserInterface/Sources/Home/HomeView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 let tagHeight = 32
 
+private struct ChipData {
+    var chipText: String
+    var isExceeded = false
+}
+
 struct HomeView: View {
     @EnvironmentObject var modelContainer: ModelContainer
     @StateObject var viewModel: HomeViewModel
-    @State var isFolding = true
+    @State var isFolding = false
     @State var isPushSearchView = false
+    @State private var chips: [[ChipData]] = [[]]
     
     var body: some View {
         mainBody
@@ -35,14 +41,47 @@ private extension HomeView {
             .frame(height: 56)
             
             ZStack {
-                tagListView
+//                tagListView
                 
                 VStack {
-                    Spacer()
-                        .frame(
-                            minHeight: 0,
-                            maxHeight: isFolding ? 58 : 58 + CGFloat((45 * viewModel.homeRows.count))
-                        )
+                    ZStack {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack(spacing: 10) {
+                                ForEach(viewModel.tags.indices, id: \.self) { index in
+                                    Text(viewModel.tags[index].tagName)
+                                        .foregroundColor(viewModel.tags[index].id == (viewModel.selectedTag?.id ?? 0) ? .white : .grey4)
+                                        .padding(.vertical, 8)
+                                        .padding(.horizontal, 12)
+                                        .background(
+                                            RoundedRectangle(cornerRadius: 4)
+                                                .foregroundColor(viewModel.tags[index].id == (viewModel.selectedTag?.id ?? 0) ? .ticlemoaBlack : .grey2)
+                                        )
+                                        .onTapGesture {
+                                            viewModel.selectedTag = viewModel.tags[index]
+                                        }
+                                }
+                            }
+                            .padding(.leading, 20)
+                            .frame(height: 60)
+                        }
+                        
+                        ZStack {
+                            HStack {
+                                Spacer()
+                                Rectangle().fill(
+                                    LinearGradient(gradient: Gradient(colors: [.clear, .white]), startPoint: .leading, endPoint: .trailing)
+                                )
+                                .frame(width: 102, height: 37)
+                            }
+                            
+                            HStack {
+                                Spacer()
+                                Image("arrow.down")
+                                    .padding(.trailing, 26.1)
+                            }
+                        }
+                    }
+                    
                     HomeArticleList(viewModel: .init(modelContainer: modelContainer))
                         .padding(.top, 0)
                         .animation(.default)
@@ -62,121 +101,155 @@ private extension HomeView {
              화면 전체에서 공용으로 사용하고 있는 articleModel의 itemPublisher에 전체 아티클들을 넣어줘야 하는데,
              다시 홈화면으로 올때, 클릭 되어 있던 태그의 아티클들을 articleModel의 itemPublisher에 넣어 UI에 표시해 주어야함
              */
-            if selectedTag.tag.tagName != "전체" {
+            if selectedTag.tagName != "전체" {
                 Task {
-                    do {
-                        try await modelContainer.articleModel.fetch(tagId: selectedTag.tag.id)
-                        print(viewModel.homeTags)
-                    } catch {
-                        print(error.localizedDescription) // TODO: 실패 토스트 메세지 띄우기
-                    }
+                    try? await modelContainer.articleModel.fetch(tagId: selectedTag.id)
                 }
             }
         }
     }
     
-    var tagListView: some View {
-        ZStack(alignment: .top) {
-            VStack(alignment: .leading, spacing: 4){
-                ForEach(Array(viewModel.homeRows.enumerated()), id:\.offset) { columnIndex, rows in
-                    
-                    HStack(spacing: 10) {
-                        ForEach(Array(rows.enumerated()), id: \.offset){ rowIndex, row in
-                            Button(
-                                action: {
-                                    HapticManager.instance.impact(style: .light)
-                                    viewModel.selectedTag = row
-                                }, label: {
-                                    Text(row.tag.tagName)
-                                }
-                            )
-                            .pretendFont(.body2)
-                            .foregroundColor(
-                                // columnIndex == 0 && rowIndex == 0
-                                viewModel.tagButtonColor(by: row)
-                            )
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 8)
-                            .background(
-                                ZStack(alignment: .trailing){
-                                    RoundedRectangle(cornerRadius: 4)
-                                        .fill(viewModel.tagBackgroundColor(by: row))
-                                }
-                            )
-                        }
-                        Spacer()
-                    }
-                    .padding(.horizontal, 20)
-                    .frame(height: 28)
-                    .padding(.vertical, 10)
-                }
-                NavigationLink(
-                    destination: {
-                        TagManagingView()
-                    },
-                    label: {
-                        
+    @ViewBuilder
+    var tagView: some View {
+        if isFolding {
+            Text("asd")
+        } else {
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 10) {
+                    ForEach(chips.indices, id: \.self) { index in
                         HStack {
-                            Text("태그 관리")
-                                .foregroundColor(.ticlemoaBlack)
-                                .customFont(
-                                    weight: 400,
-                                    size: 12,
-                                    lineHeight: 18,
-                                    style: .medium
-                                )
-                                .padding(.top, 11)
-                            
-                            Image("left_chevron")
-                                .padding(.top, 10)
-                        }.opacity(0)
-                    }
-                )
-                
-//                Spacer()
-            }
-            .padding(.top, 5)
-            .padding(.trailing, 65)
-            
-            // Fold - UnFold Button
-            VStack {
-                HStack {
-                    Spacer()
-                    Rectangle()
-                        .foregroundColor(Color.clear)
-                        .frame(width: 24, height: 24)
-                        .overlay {
-                            Image(systemName: isFolding ? "chevron.down" : "chevron.up")
-                                .frame(width: 32, height: 32)
-                                .animation(.linear)
+                            ForEach(chips[index].indices, id: \.self) { chipIndex in
+                                Text(chips[index][chipIndex].chipText)
+                                    .foregroundColor(.grey4)
+                                    .customFont(weight: 400, size: 14, lineHeight: 21)
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 12)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 4)
+                                            .foregroundColor(.grey2)
+                                    )
+                                    .lineLimit(1)
+                                    .overlay(
+                                        GeometryReader{ reader -> Color in
+                                            let maxX = reader.frame(in: .global).maxX
+                                            if maxX > UIScreen.screenWidth - 30 && !chips[index][chipIndex].isExceeded {
+                                                DispatchQueue.main.async {
+                                                    chips[index][chipIndex].isExceeded = true
+                                                    let lastItem = chips[index][chipIndex]
+                                                    chips.append([lastItem])
+                                                    chips[index].remove(at: chipIndex)
+                                                }
+                                            }
+                                            return Color.clear
+                                        },
+                                        alignment: .trailing
+                                    )
+                            }
                         }
-                    Spacer()
-                        .frame(maxWidth: 20)
+
+                    }
                 }
-                .padding(.top, 18)
-                Spacer()
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
             }
-            .onTapGesture {
-                guard viewModel.homeRows.count > 1 else { return }
-                withAnimation { isFolding.toggle() }
+            .frame(height: UIScreen.main.bounds.height / 3)
+            .onReceive(viewModel.$tags) { tags in
+                chips = [[]]
+                tags.forEach { tag in
+                    chips[chips.count - 1].append(ChipData(chipText: tag.tagName))
+                }
             }
         }
     }
-}
-
-//struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView(
-//            viewModel: HomeViewModel(
-//                modelContainer: ModelContainer(
-//                    articleModel: MockArticleModel(),
-//                    tagModel: MockTagModel(),
-//                    loginModel: MockLoginModel()
+    
+//    var tagListView: some View {
+//        ZStack(alignment: .top) {
+//            VStack(alignment: .leading, spacing: 4){
+//                ForEach(Array(viewModel.homeRows.enumerated()), id:\.offset) { columnIndex, rows in
+//
+//                    HStack(spacing: 10) {
+//                        ForEach(Array(rows.enumerated()), id: \.offset){ rowIndex, row in
+//                            Button(
+//                                action: {
+//                                    HapticManager.instance.impact(style: .light)
+//                                    viewModel.selectedTag = row
+//                                }, label: {
+//                                    Text(row.tag.tagName)
+//                                }
+//                            )
+//                            .pretendFont(.body2)
+//                            .foregroundColor(
+//                                // columnIndex == 0 && rowIndex == 0
+//                                viewModel.tagButtonColor(by: row)
+//                            )
+//                            .padding(.horizontal, 12)
+//                            .padding(.vertical, 8)
+//                            .background(
+//                                ZStack(alignment: .trailing){
+//                                    RoundedRectangle(cornerRadius: 4)
+//                                        .fill(viewModel.tagBackgroundColor(by: row))
+//                                }
+//                            )
+//                        }
+//                        Spacer()
+//                    }
+//                    .padding(.horizontal, 20)
+//                    .frame(height: 28)
+//                    .padding(.vertical, 10)
+//                }
+//                NavigationLink(
+//                    destination: {
+//                        TagManagingView()
+//                    },
+//                    label: {
+//
+//                        HStack {
+//                            Text("태그 관리")
+//                                .foregroundColor(.ticlemoaBlack)
+//                                .customFont(
+//                                    weight: 400,
+//                                    size: 12,
+//                                    lineHeight: 18,
+//                                    style: .medium
+//                                )
+//                                .padding(.top, 11)
+//
+//                            Image("left_chevron")
+//                                .padding(.top, 10)
+//                        }.opacity(0)
+//                    }
 //                )
-//            )
-//        )
+//
+////                Spacer()
+//            }
+//            .padding(.top, 5)
+//            .padding(.trailing, 65)
+//
+//            // Fold - UnFold Button
+//            VStack {
+//                HStack {
+//                    Spacer()
+//                    Rectangle()
+//                        .foregroundColor(Color.clear)
+//                        .frame(width: 24, height: 24)
+//                        .overlay {
+//                            Image(systemName: isFolding ? "chevron.down" : "chevron.up")
+//                                .frame(width: 32, height: 32)
+//                                .animation(.linear)
+//                        }
+//                    Spacer()
+//                        .frame(maxWidth: 20)
+//                }
+//                .padding(.top, 18)
+//                Spacer()
+//            }
+//            .onTapGesture {
+//                guard viewModel.homeRows.count > 1 else { return }
+//                withAnimation { isFolding.toggle() }
+//            }
+//        }
 //    }
-//}
+}
 
 extension UIScreen{
     static let screenWidth = UIScreen.main.bounds.width
