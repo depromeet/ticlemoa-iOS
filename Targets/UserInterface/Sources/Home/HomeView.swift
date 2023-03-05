067b357690734ff85c80f0318d8ba09f8a7750db
Shin <struct.developer@gmail.com>
2023-03-05 15:10:51 +0900

    feat: 홈 화면의 태그에서 불필요한 부분 제거 및 UI 다듬기
    - #250
    - 불필요한 부분 제거

--- Targets/UserInterface/Sources/Home/HomeView.swift
+++ Targets/UserInterface/Sources/Home/HomeView.swift
@@ -7,19 +7,11 @@
 
 import SwiftUI
 
-let tagHeight = 32
-
-private struct ChipData {
-    var chipText: String
-    var isExceeded = false
-}
-
 struct HomeView: View {
     @EnvironmentObject var modelContainer: ModelContainer
     @StateObject var viewModel: HomeViewModel
-    @State var isFolding = false
+    @State var isFolding = true
     @State var isPushSearchView = false
-    @State private var chips: [[ChipData]] = [[]]
     
     var body: some View {
         mainBody
@@ -33,6 +25,7 @@ private extension HomeView {
     @ViewBuilder
     var mainBody: some View {
         VStack(spacing: 0) {
+            /// TIcleMoa 로고
             HStack {
                 Image("ticlemoa_logo")
                     .padding(.leading, 20)
@@ -40,11 +33,9 @@ private extension HomeView {
             }
             .frame(height: 56)
             
-            ZStack {
-//                tagListView
-                
-                VStack(spacing: 0) {
-                    ZStack {
+            VStack(spacing: 0) {
+                ZStack {
+                    if isFolding { /// 접혀 있을 경우 일직선 배열의 tagView
                         ScrollView(.horizontal, showsIndicators: false) {
                             LazyHStack(spacing: 10) {
                                 ForEach(viewModel.tags.indices, id: \.self) { index in
@@ -64,57 +55,58 @@ private extension HomeView {
                             .padding(.leading, 20)
                             .frame(height: 60)
                         }
+                    } else { /// 펼쳐져 있을 경우 tagView
                         
-                        ZStack {
-                            HStack {
-                                Spacer()
-                                Rectangle().fill(
-                                    LinearGradient(gradient: Gradient(colors: [.clear, Color.grey1]), startPoint: .leading, endPoint: .trailing)
-                                )
-                                .frame(width: 102, height: 37)
-                            }
-                            
-                            HStack {
-                                Spacer()
-                                Button {
-                                    withAnimation {
-                                        isFolding.toggle()
-                                    }
-                                } label: {
-                                    Group {
-                                        isFolding ? Image("arrow.up") : Image("arrow.down")
-                                    }
-                                    .padding(.trailing, 26.1)
+                    }
+                    /// folding button
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            Rectangle().fill(
+                                LinearGradient(gradient: Gradient(colors: [.clear, Color.grey1]), startPoint: .leading, endPoint: .trailing)
+                            )
+                            .frame(width: 102, height: 37)
+                        }
+                        
+                        HStack {
+                            Spacer()
+                            Button {
+                                withAnimation {
+                                    isFolding.toggle()
                                 }
+                            } label: {
+                                Group {
+                                    isFolding ? Image("arrow.down") : Image("arrow.up")
+                                }
+                                .padding(.trailing, 26.1)
                             }
                         }
                     }
-                    
-                    if isFolding {
-                        NavigationLink(isActive: $viewModel.isTagManagingViewPresented) {
-                            TagManagingView()
-                        } label: {
-                            HStack(spacing: 6.82) {
-                                Text("태그 관리")
-                                    .customFont(weight: 500, size: 12, lineHeight: 18, style: .medium)
-                                Image("tagArrow.right")
-                                Spacer()
-                            }
-                            .padding(.leading, 20)
-                        }
-                        .padding(.bottom, 20)
-                    }
-                    
-                    HomeArticleList(viewModel: .init(modelContainer: modelContainer))
-                        .padding(.top, 0)
-                        .animation(.default)
-                        .transition(.slide)
-                        .environmentObject(viewModel)
-                        .background(Color.grey1)
-                    
-                    Spacer()
-                    Divider()
                 }
+                
+                /// 잡햐쟈
+                if !isFolding {
+                    NavigationLink(isActive: $viewModel.isTagManagingViewPresented) {
+                        TagManagingView()
+                    } label: {
+                        HStack(spacing: 6.82) {
+                            Text("태그 관리")
+                                .customFont(weight: 500, size: 12, lineHeight: 18, style: .medium)
+                            Image("tagArrow.right")
+                            Spacer()
+                        }
+                        .padding(.leading, 20)
+                    }
+                    .padding(.bottom, 20)
+                }
+                
+                HomeArticleList(viewModel: .init(modelContainer: modelContainer))
+                    .transition(.slide)
+                    .environmentObject(viewModel)
+                    .background(Color.grey1)
+                
+                Spacer()
+                Divider()
             }
         }
         .onAppear {
@@ -131,147 +123,6 @@ private extension HomeView {
             }
         }
     }
-    
-    @ViewBuilder
-    var tagView: some View {
-        if isFolding {
-            Text("asd")
-        } else {
-            ScrollView {
-                LazyVStack(alignment: .leading, spacing: 10) {
-                    ForEach(chips.indices, id: \.self) { index in
-                        HStack {
-                            ForEach(chips[index].indices, id: \.self) { chipIndex in
-                                Text(chips[index][chipIndex].chipText)
-                                    .foregroundColor(.grey4)
-                                    .customFont(weight: 400, size: 14, lineHeight: 21)
-                                    .padding(.vertical, 8)
-                                    .padding(.horizontal, 12)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 4)
-                                            .foregroundColor(.grey2)
-                                    )
-                                    .lineLimit(1)
-                                    .overlay(
-                                        GeometryReader{ reader -> Color in
-                                            let maxX = reader.frame(in: .global).maxX
-                                            if maxX > UIScreen.screenWidth - 30 && !chips[index][chipIndex].isExceeded {
-                                                DispatchQueue.main.async {
-                                                    chips[index][chipIndex].isExceeded = true
-                                                    let lastItem = chips[index][chipIndex]
-                                                    chips.append([lastItem])
-                                                    chips[index].remove(at: chipIndex)
-                                                }
-                                            }
-                                            return Color.clear
-                                        },
-                                        alignment: .trailing
-                                    )
-                            }
-                        }
-
-                    }
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
-            }
-            .frame(height: UIScreen.main.bounds.height / 3)
-            .onReceive(viewModel.$tags) { tags in
-                chips = [[]]
-                tags.forEach { tag in
-                    chips[chips.count - 1].append(ChipData(chipText: tag.tagName))
-                }
-            }
-        }
-    }
-    
-//    var tagListView: some View {
-//        ZStack(alignment: .top) {
-//            VStack(alignment: .leading, spacing: 4){
-//                ForEach(Array(viewModel.homeRows.enumerated()), id:\.offset) { columnIndex, rows in
-//
-//                    HStack(spacing: 10) {
-//                        ForEach(Array(rows.enumerated()), id: \.offset){ rowIndex, row in
-//                            Button(
-//                                action: {
-//                                    HapticManager.instance.impact(style: .light)
-//                                    viewModel.selectedTag = row
-//                                }, label: {
-//                                    Text(row.tag.tagName)
-//                                }
-//                            )
-//                            .pretendFont(.body2)
-//                            .foregroundColor(
-//                                // columnIndex == 0 && rowIndex == 0
-//                                viewModel.tagButtonColor(by: row)
-//                            )
-//                            .padding(.horizontal, 12)
-//                            .padding(.vertical, 8)
-//                            .background(
-//                                ZStack(alignment: .trailing){
-//                                    RoundedRectangle(cornerRadius: 4)
-//                                        .fill(viewModel.tagBackgroundColor(by: row))
-//                                }
-//                            )
-//                        }
-//                        Spacer()
-//                    }
-//                    .padding(.horizontal, 20)
-//                    .frame(height: 28)
-//                    .padding(.vertical, 10)
-//                }
-//                NavigationLink(
-//                    destination: {
-//                        TagManagingView()
-//                    },
-//                    label: {
-//
-//                        HStack {
-//                            Text("태그 관리")
-//                                .foregroundColor(.ticlemoaBlack)
-//                                .customFont(
-//                                    weight: 400,
-//                                    size: 12,
-//                                    lineHeight: 18,
-//                                    style: .medium
-//                                )
-//                                .padding(.top, 11)
-//
-//                            Image("left_chevron")
-//                                .padding(.top, 10)
-//                        }.opacity(0)
-//                    }
-//                )
-//
-////                Spacer()
-//            }
-//            .padding(.top, 5)
-//            .padding(.trailing, 65)
-//
-//            // Fold - UnFold Button
-//            VStack {
-//                HStack {
-//                    Spacer()
-//                    Rectangle()
-//                        .foregroundColor(Color.clear)
-//                        .frame(width: 24, height: 24)
-//                        .overlay {
-//                            Image(systemName: isFolding ? "chevron.down" : "chevron.up")
-//                                .frame(width: 32, height: 32)
-//                                .animation(.linear)
-//                        }
-//                    Spacer()
-//                        .frame(maxWidth: 20)
-//                }
-//                .padding(.top, 18)
-//                Spacer()
-//            }
-//            .onTapGesture {
-//                guard viewModel.homeRows.count > 1 else { return }
-//                withAnimation { isFolding.toggle() }
-//            }
-//        }
-//    }
 }
 
 extension UIScreen{
@@ -283,7 +134,6 @@ extension String{
         let font = UIFont.systemFont(ofSize: 16)
         let attributes = [NSAttributedString.Key.font: font]
         let size = (self as NSString).size(withAttributes: attributes)
-        // 24 : leading trailing default padding value
-        return size.width + 24
+        return size.width
     }
 }
